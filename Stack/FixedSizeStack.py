db3def9e44504258a34f891606957485a4f881dd
Gabriel Gonçalves <140024682+Smeltier@users.noreply.github.com>
2025-03-10 21:37:08 -0300

    Create FixedSizeStack.py

---
+++ Stack/FixedSizeStack.py
@@ -0,0 +1,38 @@
+class FixedSizeStack:
+    def __init__(self, max_length = 1000):
+        self.items = [0] * max_length
+        self.max_length = max_length
+        self.pointer = 0
+
+    def push(self, item):
+        if self.pointer == self.max_length:
+            raise IndexError("Stack Overflow")
+        self.items[self.pointer] = item
+        self.pointer += 1
+
+    def is_full(self):
+        return self.pointer == self.max_length
+
+    def is_empty(self):
+        return self.pointer == 0
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("Error, your stack is empty")
+        self.pointer -= 1
+        return self.items[self.pointer]
+    
+    def top(self):
+        if self.is_empty():
+            raise IndexError("Error, your stack is empty")
+        return self.items[self.pointer - 1]
+    
+    def size(self):
+        return self.pointer
+    
+    def clear(self):
+        self.pointer = 0
+        self.items = [0] * self.max_length
+    
+    def contains(self, item):
+        return item in self.items[:self.pointer]
