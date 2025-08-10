0099834af19723314e926924d5aa43d349d29862
Gabriel Gonçalves <140024682+Smeltier@users.noreply.github.com>
2025-08-10 16:45:47 -0300

    Update Stack.py

--- src/stack/Stack.py
+++ src/stack/Stack.py
@@ -1,25 +1,30 @@
+from typing import Any
+
 class Stack:
     def __init__(self):
-        self.items = []
+        self.items: list[Any] = []
 
-    def push(self,item):
+    def __repr__(self) -> str:
+        return f'Stack = {self.items}'
+
+    def push(self, item: Any) -> None:
         self.items.append(item)
 
-    def size(self):
+    def size(self) -> int:
         return len(self.items)
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return len(self.items) == 0
 
-    def pop(self):
+    def pop(self) -> Any:
         if self.is_empty():
             raise IndexError("Error, your Stack is empty.")
         return self.items.pop()
     
-    def top(self):
+    def top(self) -> Any:
         if self.is_empty():
             raise IndexError("Error, your Stack is empty.")
         return self.items[-1]
     
-    def clear(self):
-        self.items = []
+    def clear(self) -> None:
+        self.items.clear()
